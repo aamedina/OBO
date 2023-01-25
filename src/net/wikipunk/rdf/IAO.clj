341371c0ce94fafb0ef83ba6ab34fff400a122e7
Adrian Medina <adrian@wikipunk.net>
2023-01-25 17:41:15 -0500

    BFO alignment and add FMA

--- src/net/wikipunk/rdf/IAO.clj
+++ src/net/wikipunk/rdf/IAO.clj
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5ee250fd90028c2eeb3b13993760fa6ba23e3fbbdbef976399f841d79f994da
-size 323161
+oid sha256:7d69435df4eed335ed369cd6bc6ce828fd4e1713ff891aad5440ad4f7fad888d
+size 324221
