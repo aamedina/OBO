5cd78d9e687f10b6175fb6fb2d7a0f1b7fe9e221
Adrian Medina <adrian@wikipunk.net>
2023-01-26 16:31:45 -0500

    gardening

--- src/net/wikipunk/rdf/TO.clj
+++ src/net/wikipunk/rdf/TO.clj
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bfec580b7fb0c546f9136f89da0f51bbed90604cc443edb82b4d40b9a86cfe1
-size 1002799
+oid sha256:ab76a904b8ec7dd28e538e65b7713cb002b40d9128e75f9bc34b1d210fbdc53e
+size 997324
