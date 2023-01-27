14db7b1080a1184af848b9c06e9493033d7b4969
Adrian Medina <adrian@wikipunk.net>
2023-01-27 16:27:19 -0500

    BFO

--- src/net/wikipunk/rdf/BFO.clj
+++ src/net/wikipunk/rdf/BFO.clj
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12ab08265c5063414242bac29a6c86150f89c0fe6c6e7927de6b9ed0f8f872c2
-size 174767
+oid sha256:747ec9e56abae64842dc896fa031012fc779d670ec9e9bb299186f6a90f168cf
+size 174323
--- src/net/wikipunk/rdf/REO.clj
+++ src/net/wikipunk/rdf/REO.clj
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03595af87b4af5704ff50abd5c3623282bbb46f0378bd5cf0969896e1efc55f3
-size 2697176
+oid sha256:352cab8fd31a3cbcbbe21a712f80e0b6b20ca72cead97b004504e4dbd25649e0
+size 2692819
