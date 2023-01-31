22922828c0e0e47a70aef130f8e6d5d080fb43ba
Adrian Medina <adrian@wikipunk.net>
2023-01-31 12:24:09 -0500

    SWO was generated with :swo/ prefix instead of :obo/

--- src/net/wikipunk/rdf/SWO.clj
+++ src/net/wikipunk/rdf/SWO.clj
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cf677e4d3940622e52fae29c34b3ca48cd34871d6226b862c40109686d95cb6
-size 1345578
+oid sha256:1872cb36bbde52b72fde2c39fe10371da222a976d43543ecc751566df8ee12d3
+size 1330959
